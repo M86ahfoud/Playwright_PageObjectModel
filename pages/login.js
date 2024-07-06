6f45a3ce0f434291fb2ef347af7753674d7fbccb
FIRST\_Mahfoud LAST\_HANI <[MY\_NAME@example.com](mailto:hanimahfoud@gmail.com)>
2024-07-06 16:34:53 +0200

    dataFixture

--- pages/login.js
+++ pages/login.js
@@ -1,11 +1,12 @@
+import selecteur from "../fixture/selecteur.json"
 exports.LoginPage = class LoginPage {
-
+    
     constructor(page) {
 
         this.page = page
-        this.username_textbox = page.getByLabel('Username')
-        this.password_textox = page.getByLabel('Password')
-        this.login_button = page.getByRole('button', { name: ' Login' })
+        this.username_textbox = page.getByLabel(selecteur.customerLogin.username)
+        this.password_textox = page.getByLabel(selecteur.customerLogin.password)
+        this.login_button = page.locator(selecteur.customerLogin.login_button)
     }
     async gotoLoginPage(url) {
         await this.page.goto(url);
--- tests/demo/login.spec.js
+++ tests/demo/login.spec.js
@@ -1,12 +1,13 @@
 import { test, expect } from '@playwright/test';
 import {LoginPage} from '../../pages/login'
-
+import url from '../../fixture/url.json'
+import account from '../../fixture/account.json'
 
 test('test', async ({ page }) => {
 
   const Login = new LoginPage(page);
   
-  await Login.gotoLoginPage('https://the-internet.herokuapp.com/login')
-  await Login.login('tomsmith','SuperSecretPassword!')
+  await Login.gotoLoginPage(url.urlSite)
+  await Login.login(account.customer.username, account.customer.password)
   
 });
